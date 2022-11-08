c1b8ed1f1dc5ee88ac1251c909e9618cca634210
4310V343k <56449763+4310V343k@users.noreply.github.com>
2022-11-09 00:41:08 +0300

    the settings are now taken from the environment also slight changes to the configs

--- remanga/settings.py
+++ remanga/settings.py
@@ -1,27 +1,30 @@
 import os
 from pathlib import Path
 
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+DEBUG = env('DEBUG')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+SECRET_KEY = env('SECRET_KEY')
 
 ALLOWED_HOSTS = [
-    '0.0.0.0'
+    '*'
 ]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'remanga.api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

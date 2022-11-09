49e14a640ea288c9d70280dbe7dfd4c7cb562d8b
4310V343k <56449763+4310V343k@users.noreply.github.com>
2022-11-09 20:20:06 +0300

    move the root a bit

--- mangalib/urls.py
+++ mangalib/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('', lambda request: redirect('admin/', permanent=True)),
     path('admin/', admin.site.urls),
-    path('', include('mangalib.api.urls')),
+    path('api/', include('mangalib.api.urls')),
 ]

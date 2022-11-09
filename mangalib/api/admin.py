bdd221dc2d3f02b0968fc87bc0df0a9ee541e354
4310V343k <56449763+4310V343k@users.noreply.github.com>
2022-11-09 17:39:52 +0300

    cool admin panel bells and whistles included

--- mangalib/api/admin.py
+++ mangalib/api/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from mangalib.api.models import Title, Volume, Chapter
+
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    exclude = ['watch_count', 'like_count']
+    pass
+
+
+class ChapterInline(admin.StackedInline):
+    model = Chapter
+    extra = 0
+    show_change_link = True
+
+
+@admin.register(Volume)
+class VolumeAdmin(admin.ModelAdmin):
+
+    inlines = [ChapterInline]
+
+
+class VolumeInline(admin.StackedInline):
+    model = Volume
+    extra = 0
+    show_change_link = True
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    search_fields = [
+        'russian_name', 'english_name',
+        'alternative_names', 'description',
+        'volumes__name', 'volumes__chapters__name'
+    ]
+    inlines = [VolumeInline]
+
